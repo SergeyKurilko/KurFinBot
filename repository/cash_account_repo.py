ee9e811bd9554f182755d8eeaadcab3ef896dea7
Sergey <kursevlad@yandex.ru>
2026-04-15 19:58:22 +0300

    new: add CashAccountRepository

---
+++ repository/cash_account_repo.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+from database.models import CashAccount
+
+class CashAccountRepository:
+    "Репозиторий для работы с CashAccount"
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_new_account(self, title: str, balance: int, currency: str = "RUB"):
+        new_cash_account = CashAccount(
+            title=title,
+            balance=balance,
+            currency=currency
+        )
+        self.session.add(new_cash_account)
+        await self.session.flush()
+        await self.session.refresh(new_cash_account)
+        return new_cash_account
